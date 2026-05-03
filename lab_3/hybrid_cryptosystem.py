d733997a28bd2514196cbf5de6c2eb009ce9d561
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2026-05-03 18:31:19 +0400

    created program skeleton

---
+++ lab_3/hybrid_cryptosystem.py
@@ -0,0 +1,52 @@
+import argparse
+import json
+
+import modules_folder.key_generation as key_generation
+import modules_folder.encryption as encryption
+import modules_folder.decryption as decryption
+
+settings = {
+    'initial_file': 'data/initial_text.txt',
+    'encrypted_file': 'data/encrypted_text.txt',
+    'decrypted_file': 'data/dectypted_text.txt',
+    'symmetric_key': 'data/symmetric_key.txt',
+    'public_key': 'data/public_key.pem',
+    'secret_key': 'data/secret_key.pem',
+}
+
+# пишем в файл
+with open('settings.json', 'w') as fp:
+    json.dump(settings, fp)
+
+
+def argument_parsing():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-gen', '--generation',
+                       action='store_true',
+                       help='Запускает режим генерации ключей')
+    group.add_argument('-enc', '--encryption',
+                       action='store_true',
+                       help='Запускает режим шифрования')
+    group.add_argument('-dec', '--decryption',
+                       action='store_true',
+                       help='Запускает режим дешифрования')
+
+    args = parser.parse_args()
+
+    # читаем из файла
+    with open('settings.json') as json_file:
+        json_data = json.load(json_file)
+
+    if args.generation:
+        key_generation.key_gen_and_save(json_data)
+    elif args.encryption:
+        # шифруем
+        pass
+    elif args.decryption:
+        # дешифруем
+        pass
+
+
+if __name__ == '__main__':
+    argument_parsing()
---
+++ lab_3/modules_folder/key_generation.py
@@ -0,0 +1,75 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+import os  # можно обойтись стандартным модулем
+
+
+def symmetric_key_generate() -> str:
+    # генерация ключа симметричного алгоритма шифрования
+    key = os.urandom(32)  # это байты
+    print("Symmetric key generated!")
+    print("Type of key: ", type(key))
+    print("Key:", key)
+    return key
+
+
+def asymmetric_key_generate():
+    # генерация пары ключей для асимметричного алгоритма шифрования
+    keys = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    private_key = keys
+    public_key = keys.public_key()
+
+    print("private key type:", type(private_key))
+    print("private key:", private_key)
+    print("public key type:", type(public_key))
+    print("private key:", public_key)
+    return [private_key, public_key]
+
+
+def symm_key_serialize(filepath: str, key: str) -> None:
+    with open(filepath, 'wb') as key_file:
+        key_file.write(key)
+
+
+def public_key_serialize(public_pem_path: str, public_key):
+    # сериализация открытого ключа в файл
+    with open(public_pem_path, 'wb') as public_out:
+        public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                 format=serialization.PublicFormat.SubjectPublicKeyInfo))
+
+
+def private_key_serialize(private_pem_path: str, private_key):
+    # сериализация закрытого ключа в файл
+    with open(private_pem_path, 'wb') as private_out:
+        private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                    format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                    encryption_algorithm=serialization.NoEncryption()))
+
+
+def RSA_encryption(text: str, public_key):
+    # шифрование текста при помощи RSA-OAEP (это усиливающая классический RSA cхема с использованием двух криптостойких хеш-функций и паддинга, если интересно, можно почитать здесь https://habr.com/ru/post/99376/)
+    text = bytes(text)
+    c_text = public_key.encrypt(text, padding.OAEP(mgf=padding.MGF1(
+        algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+
+    print("RSA encryption successful!")
+    return c_text
+
+
+def key_gen_and_save(json_data):
+    # symmetric key gen and save
+    symm_key = symmetric_key_generate()
+    symm_key_serialize(json_data['symmetric_key'], symm_key)
+
+    # asymm keys gen and save
+    private_key, public_key = asymmetric_key_generate()
+    public_key_serialize(json_data['public_key'], public_key)
+    private_key_serialize(json_data['secret_key'], private_key)
+
+    # encryption of symm_key
+    crypted_symm_key = RSA_encryption(symm_key, public_key)
